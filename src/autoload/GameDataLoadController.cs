464b0702edf552cbbb19cbc2f16682eeb1308c7e
Jayden Tavares <jayden.tavares@hotmail.com>
2023-10-29 12:59:36 +1100

    add config game data and game state data on load

--- src/autoload/GameDataLoadController.cs
+++ src/autoload/GameDataLoadController.cs
@@ -8,8 +8,13 @@ public partial class GameDataLoadController : Node
     public void Load(GameData gameData, GameStateData gameStateData)
     {
         GD.Print($"GameDataController: Load(): Loading game [{gameData.ResourceName}], game state [{gameStateData.ResourceName}]");
-
         GameData = gameData;
         GameStateData = gameStateData;
+
+        // set config game data and game state data on load
+        ConfigData configData = GetNode<ConfigController>("/root/ConfigController").ConfigData;
+        configData.GameStateData = GameStateData;
+        configData.GameData = GameData;
+        ResourceSaver.Save(configData);
     }
 }
