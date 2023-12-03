c6e8d498b2e8fea0ad6143630c17ccee827bc122
Jayden Tavares <jayden.tavares@hotmail.com>
2023-12-03 22:35:52 +1100

    add exiting in game and battle game modes (go back to previous game mode)

--- src/scenes/ui/screens/battle/BattleScreen.cs
+++ src/scenes/ui/screens/battle/BattleScreen.cs
@@ -2,5 +2,10 @@ using Godot;
 
 public partial class BattleScreen : Screen
 {
-
+    public override void Exit()
+    {
+        // change game mode to class_select
+        GameModeData changeGameMode = GameModeData.ByName("in_game");
+        GetNode<GameModeController>("/root/GameModeController").ChangeGameMode(changeGameMode);
+    }
 }
--- src/scenes/ui/screens/in_game/InGameScreen.cs
+++ src/scenes/ui/screens/in_game/InGameScreen.cs
@@ -2,6 +2,13 @@ using Godot;
 
 public partial class InGameScreen : Screen
 {
+    public override void Exit()
+    {
+        // change game mode to class_select
+        GameModeData changeGameMode = GameModeData.ByName("class_select");
+        GetNode<GameModeController>("/root/GameModeController").ChangeGameMode(changeGameMode);
+    }
+
     private void onBattleButtonPressed()
     {
         GetNode<BattleController>("/root/BattleController").StartBattle();
